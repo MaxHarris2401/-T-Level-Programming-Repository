d2c020a93b4f41711000a3f3465748cfe8318517
Max <031269@cirencester.ac.uk>
2024-09-12 10:18:30 +0100

    Lesson 3

--- Lesson 2/Q34 ProgrammingChallenges.py
+++ Lesson 2/Q34 ProgrammingChallenges.py
@@ -1,13 +1,17 @@
-Temp = int(input("Enter the current Temperature in centigrade: "))
-if Temp < 0:
-    print("Its Freezing")
-elif Temp >=0 and Temp <=10:
-    print("Its very cold")
-elif Temp >=10 and Temp <=20:
-    print("Its cold")
-elif Temp >=20 and Temp <=30:
-    print("Its normal")
-elif Temp >=30 and Temp <=40:
-    print("Its hot")
-elif Temp >=40:
-    print("Its very hot")
+Temp = input("Enter the current Temperature in centigrade: ")
+if str.isdigit(Temp):
+    Num=int(Temp)
+    if Num < 0:
+        print("Its Freezing")
+    elif Num >=0 and Num <=10:
+        print("Its very cold")
+    elif Num >=10 and Num <=20:
+        print("Its cold")
+    elif Num >=20 and Num <=30:
+        print("Its normal")
+    elif Num >=30 and Num <=40:
+        print("Its hot")
+    elif Num >=40:
+        print("Its very hot")
+else:
+    print("This is not a number")
