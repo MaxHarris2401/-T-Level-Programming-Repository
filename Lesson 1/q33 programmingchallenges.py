f2df6fcdc27971dbd0ca05472ccb26552ba54095
Max <031269@cirencester.ac.uk>
2024-09-19 11:33:59 +0100

    scrabble

--- Lesson 1/q33 programmingchallenges.py
+++
@@ -1,7 +0,0 @@
-marksMath=int(input("Math marks "))
-marksPhy=int(input("Physic marks "))
-marksChem=int(input("Chemust marks "))
-if marksMath >=65 and marksPhy >=55 and marksChem>=50 and(marksMath+marksPhy+marksChem)>=180:
-    print("your barred mate")
-else:
-    print("your in son")
