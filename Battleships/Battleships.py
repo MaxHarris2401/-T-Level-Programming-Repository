f664f40bf02e28728375024e54ef0cf967006a73
Max <031269@cirencester.ac.uk>
2024-11-06 14:05:32 +0000

    Battleships

---
+++ Battleships/Battleships.py
@@ -0,0 +1,26 @@
+board = [] # initialise board array
+level = 1
+def main():
+    print("----------Battleships-----------\nPress any key to play")
+    key_press_menu = input(">>> ")
+    setup_board()
+    display_board()
+
+def setup_board():
+    global board
+    global level
+    for i in range(10):
+        board.append(["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"])
+    if (level==1):
+
+def display_board():
+    print("  0   1   2   3   4   5   6   7   8   9")
+    for number, row in enumerate(board):
+        print("   -+---+---+---+---+---+---+---+---+-")
+        print(number, " | ".join(row))
+        
+def player_turn():
+    col = int(input("Please enter a column"))
+    if col 
+    row = int(input("Please enter a row"))
+main()
