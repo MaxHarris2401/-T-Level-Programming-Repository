06806d5c7eec5a21a75d2d3ae2235b56ea9594c8
Max <031269@cirencester.ac.uk>
2024-11-07 11:18:46 +0000

    Battleships update

--- Battleships/Battleships.py
+++ Battleships/Battleships.py
@@ -1,4 +1,5 @@
 board = [] # initialise board array
+empty_board = []
 level = 1
 hits = 0
 turns = 0
@@ -12,7 +13,11 @@ def main():
 
 def setup_board():
     global board
+    global empty_board
     global level
+    level = int(input("Enter a level (1-3): "))
+    if level < 1 or level > 3:
+        print("Invalid value entered, try again")
     for i in range(10):
         board.append(["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"])
     if level == 1:
@@ -32,47 +37,90 @@ def setup_board():
         for i in range(3):
             board[6][7+count] = "C"
             count = count+1
-
+    
+    elif level == 2:
+        count = 0
+        for i in range(5):
+            board[5+count][0] = "A"
+            count = count+1
+        count = 0
+        for i in range(4):
+            board[7][2+count] = "B"
+            count = count+1
+        count = 0
+        for i in range(3):
+            board[6][3+count] = "S"
+            count = count+1
+        count = 0
+        for i in range(3):
+            board[1+count][9] = "C"
+            count = count+1
+    
+    elif level == 3:
+        count = 0
+        for i in range(5):
+            board[2+count][8] = "A"
+            count = count+1
+        count = 0
+        for i in range(4):
+            board[4+count][3] = "B"
+            count = count+1
+        count = 0
+        for i in range(3):
+            board[4][5+count] = "S"
+            count = count+1
+        count = 0
+        for i in range(3):
+            board[3+count][4] = "C"
+            count = count+1
 
 def display_board():
     print("  0   1   2   3   4   5   6   7   8   9")
     for number, row in enumerate(board):
-        print("   -+---+---+---+---+---+---+---+---+-")
         print(number, " | ".join(row))
+        print("   -+---+---+---+---+---+---+---+---+-")
         
 def player_turn():
     global hits
     global turns
-    while True:
-        str_col = input("Please enter a column (0-9): ")
-        try:
-            int_col = int(str_col)
-            if int_col < 0 or int_col > 10:
-                print("Invalid value entered, please try again")
-            else:
-                break
-        except:
-            print("Invalid data type entered, please try again")
-    while True:
-        str_row = int(input("Please enter a row (0-9): "))
-        try:
-            int_row = int(str_row)
-            if int_row < 0 or int_row > 10:
-                print("Invalid value entered, please try again")
-            else:
-                break
-        except:
-            print("Invalid data type entered, please try again")
-    if board[int_col][int_row] == "A" or board[int_col][int_row] == "B" or \
-    board[int_col][int_row] == "C" or board[int_col][int_row] == "S":
-        print("Hit")
-        board[int_col][int_row] = "*"
-        hits+=1
-        turns+=1
-    else:
-        print("Miss")
-        board[int_col][int_row] = "."
-        turns+=1
+    while game_over != True:
+        while True:
+            str_col = input("Please enter a column (0-9): ")
+            try:
+                int_col = int(str_col)
+                if int_col < 0 or int_col > 10:
+                    print("Invalid value entered, please try again")
+                else:
+                    break
+            except:
+                print("Invalid data type entered, please try again")
+        while True:
+            str_row = int(input("Please enter a row (0-9): "))
+            try:
+                int_row = int(str_row)
+                if int_row < 0 or int_row > 10:
+                    print("Invalid value entered, please try again")
+                else:
+                    break
+            except:
+                print("Invalid data type entered, please try again")
+        if board[int_col][int_row] == "A" or board[int_col][int_row] == "B" or \
+        board[int_col][int_row] == "C" or board[int_col][int_row] == "S":
+            print("Hit")
+            board[int_col][int_row] = "*"
+            hits+=1
+            turns+=1
+        else:
+            print("Miss")
+            board[int_col][int_row] = "."
+            turns+=1
+        display_board()
+        if game_over():
+            print("Game over as number of turns has exceeded")
+            exit()
+        if has_won():
+            print("Game over as player won")
+            exit()
 
 def has_won():
     global hits
@@ -87,6 +135,5 @@ def game_over():
         return True
     else:
         return False
+
 main()
-if game_over():
-    print("Game over as number of turns has exceeded")
